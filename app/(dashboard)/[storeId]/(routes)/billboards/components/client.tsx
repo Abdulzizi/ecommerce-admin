cfed3db20b69ba922625fa9a8d98758007dbae68
Abdul Jawad Azizi <jawadazizi052@gmail.com>
2024-03-24 13:29:33 +0700

    Billboard page 100% done

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -7,6 +7,7 @@ import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { BillboardColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps {
   data: BillboardColumn[];
@@ -34,6 +35,11 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
       <Separator />
 
       <DataTable searchKey="label" columns={columns} data={data} />
+
+      <Separator />
+
+      <Heading title="API Routes" description="Api calls for billboards" />
+      <ApiList entityName="billboards" entityIdName="billboardId" />
     </>
   );
 };
